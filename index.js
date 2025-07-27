76bc45b2276921c9008ab6a9d223784c3ebd49b9
Seinz-design <seinz.creator@gmail.com>
2025-07-28 01:14:55 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+let alamatFooter = document.getElementById('alamatFooter')
+
+alamatFooter.innerHTML = 
+'<h3 class="ps-4">Alamat</h3>'+
+'<p class="ms-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis nesciunt blanditiis illum sed, voluptates tenetur!</p>';
